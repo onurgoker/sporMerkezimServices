b794e5373e56e491eb867b443ecb26b3418c3e27
Onur Göker <114473362+onurgokerlotec@users.noreply.github.com>
2023-08-30 09:21:41 +0300

    Initial service is added

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    #[Route('/lucky/number')]
+    /**
+     * Summary of number
+     * @return Symfony\Component\HttpFoundation\Response
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->json(['property'=>$number],200);
+    }
+}
